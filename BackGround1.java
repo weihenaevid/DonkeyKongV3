dc089d7231bbbc706219a186580556bef65663f9
weihenaevid <vidunidw25@gmail.com>
2020-01-16 14:32:56 -0500

    changed the position

--- BackGround1.java
+++ BackGround1.java
@@ -37,6 +37,7 @@ public class BackGround1 extends World
         addObject(new Floor2(), 1375, 1000);
         
         addObject(new Mario(), 1050, 940);
+        addObject(new Coin(), 500,900);
         
         GreenfootImage lives = new GreenfootImage("heart.png");
         GreenfootImage explostion = new GreenfootImage("explosion.png");
